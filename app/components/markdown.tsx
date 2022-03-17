9ce0cc862bc612a0db6105dbfe18a1864eada330
Felix Angelov <felangelov@gmail.com>
2022-03-17 08:35:26 -0500

    fix: markdown prose invert (#4)

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -3,7 +3,7 @@ import { marked } from "marked";
 export function Markdown({ content }: { content: string }) {
   return (
     <article
-      className="prose dark:prose-invert"
+      className="prose prose-invert"
       dangerouslySetInnerHTML={{ __html: marked(content) }}
     ></article>
   );
