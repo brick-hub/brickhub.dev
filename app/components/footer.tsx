6872eed27ae6db696089dc0e69871c2dbd21859b
Felix Angelov <felangelov@gmail.com>
2022-10-09 22:29:10 -0500

    feat: reference docs.brickhub.dev

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -27,6 +27,14 @@ export function Footer() {
         >
           <ExclamationCircleIcon />
         </FooterLink>
+        <FooterLink
+          aria-label="Read the docs"
+          target="_blank"
+          rel="noreferrer"
+          href="https://docs.brickhub.dev"
+        >
+          <DocumentTextIcon />
+        </FooterLink>
       </nav>
     </footer>
   );
@@ -49,6 +57,27 @@ FooterLink.displayName = "FooterLink";
 function BrickHubLogo() {
   return (
     <img alt="Brick Hub" src="/images/brickhub.svg" width="40" height="38" />
+  );
+}
+
+function DocumentTextIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-7 w-7"
+      aria-hidden="true"
+    >
+      <title>Read the Docs</title>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+      />
+    </svg>
   );
 }
 
