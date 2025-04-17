b83c9e58099c13ed5f70a269d5c163d97169e003
Felix Angelov <felangelov@gmail.com>
2025-04-16 20:43:41 -0500

    feat: include policy link in footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -33,6 +33,9 @@ export function Footer() {
           rel="noreferrer"
           href="https://docs.brickhub.dev"
         >
+          <BookOpenIcon />
+        </FooterLink>
+        <FooterLink aria-label="Read the policy" href="/policy">
           <DocumentTextIcon />
         </FooterLink>
       </nav>
@@ -71,7 +74,7 @@ function DocumentTextIcon() {
       className="h-7 w-7"
       aria-hidden="true"
     >
-      <title>Read the Docs</title>
+      <title>Read the policy</title>
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -116,6 +119,26 @@ function ExclamationCircleIcon() {
         strokeWidth="2"
         d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
       ></path>
+    </svg>
+  );
+}
+
+function BookOpenIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-7 w-7"
+    >
+      <title>Read the docs</title>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+      />
     </svg>
   );
 }
--- app/routes/policy.tsx
+++ app/routes/policy.tsx
@@ -1,8 +1,43 @@
 import { redirect } from "@remix-run/node";
+import Markdoc from "@markdoc/markdoc";
 import type { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Fragment } from "react";
+import { Footer, Header } from "~/components";
+import { useOptionalUser } from "~/utils/user";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  return redirect(
-    "https://github.com/brick-hub/brickhub.dev/blob/main/docs/policy.md"
+  const response = await fetch(
+    "https://raw.githubusercontent.com/brick-hub/brickhub.dev/refs/heads/main/docs/policy.md"
   );
+
+  // Fallback to previous behavior.
+  if (response.status != 200) {
+    return redirect(
+      "https://github.com/brick-hub/brickhub.dev/blob/main/docs/policy.md"
+    );
+  }
+
+  const doc = await response.text();
+  const ast = Markdoc.parse(doc);
+  const content = Markdoc.transform(ast);
+  const html = Markdoc.renderers.html(content);
+  return { html };
 };
+
+export default function Policy() {
+  const { html } = useLoaderData<typeof loader>();
+  const user = useOptionalUser();
+  return (
+    <Fragment>
+      <Header user={user} />
+      <main className="flex h-3/4 flex-1 flex-col">
+        <div
+          className="prose prose-invert mx-auto w-11/12 py-6 xl:w-10/12 2xl:w-[1280px]"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </main>
+      <Footer />
+    </Fragment>
+  );
+}
